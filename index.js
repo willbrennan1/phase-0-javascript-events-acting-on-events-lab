5753d47f506de70fc82eac28e6679b11b0390eb5
Will Brennan <willbrennan314@gmail.com>
2021-11-09 08:44:27 -0500

    commit tree 05a3e4c901b9e19304b35b195fed16201db305e2

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
+// console.log(dodger);
+
+dodger.style.backgroundColor = "#FF69B4";
+// dodger.style.bottom = "0px";
+// dodger.style.left = "100px";
+// dodger.style.right = "0px";
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+    });
+
+function moveDodgerLeft() {
+
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
